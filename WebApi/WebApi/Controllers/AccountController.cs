338f4cfd184a78702da629a79ccb9dd4e9b06a50
Oliver Reimer Maddams <omaddams@hotmail.com>
2024-08-21 12:21:05 +0200

    me endpoint

---
+++ WebApi/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userDTO = new
+            {
+                user.Id,
+                user.UserName,
+                user.Email,
+            };
+
+            return Ok(userDTO);
+        }
+    }
+}
