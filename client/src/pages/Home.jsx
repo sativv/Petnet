845f78e0ed84432a3e5e69d8202ecf415d85c15d
Oliver Reimer Maddams <omaddams@hotmail.com>
2024-08-27 14:32:30 +0200

    basic add display container

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const nav = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+    var response = fetch("http://localhost:5054/api/post", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(response => response.json())
+    .then(result => setPosts(result))
+    .then(setLoading(false));
+
+    console.log(posts);
+  }, [])
+
+  
+
   return (
     <div>
-      <button onClick={() => nav("/login")}>
-        Klicka på mig för att logga in
-      </button>
+      <div className="post-container">
+        {loading && <p>Loading posts..</p>}
+        {posts.map((post) => {
+          //<Post />
+        }) }
+      </div>
     </div>
   );
 }
