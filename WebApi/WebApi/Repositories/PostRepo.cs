cd571fbd05bc81f13b2e30faffc683256dd9185f
David Vitas <david.vitas@hotmail.com>
2024-09-02 16:53:17 +0200

    changed postupdate endpoint

--- WebApi/WebApi/Repositories/PostRepo.cs
+++ WebApi/WebApi/Repositories/PostRepo.cs
@@ -11,7 +11,7 @@ namespace WebApi.Repositories
 
         public PostRepo(ApplicationDbContext context)
         {
-            _context = context; 
+            _context = context;
         }
 
         // Get one post 
@@ -38,13 +38,17 @@ namespace WebApi.Repositories
             await _context.Posts.AddAsync(postModelToAdd);
             return postModelToAdd;
         }
-        // Update post 
+
+
         public async Task<PostModel> UpdatePostAsync(PostModel postToUpdate)
         {
-            var currentPost = await GetPostAsync(postToUpdate.Id);
-            currentPost = postToUpdate;
-            return currentPost;
+            _context.Entry(postToUpdate).State = EntityState.Modified;
+
+            return postToUpdate;
         }
+
+
+
 
 
         // Remove post 
