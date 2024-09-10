70cf13cabb3f2b3badf27a3d0496a6e13055be11
EmmaDQ <sydemma@hotmail.com>
2024-09-10 15:35:16 +0200

    add cypress test log out

--- client/cypress/e2e/logout.cy.js
+++ client/cypress/e2e/logout.cy.js
@@ -1,4 +1,9 @@
-describe("User can logout if logged in", () => {
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // Return false för att förhindra att testet misslyckas
+  return false;
+});
+
+describe("User can logout if logged in desktop version", () => {
   it("should not have a logout button if not logged in", () => {
     cy.visit("http://localhost:3000");
 
@@ -14,26 +19,52 @@ describe("User can logout if logged in", () => {
     // Klicka på "Logga in"-knappen
     cy.contains("Logga in").click();
     cy.wait(800);
+    cy.url().should("include", "/login");
     cy.contains("Logga in");
-
-    //  cy.get(".burgerIcon").click();
-    //  cy.get('.navbarMenu > [href="/login"]').should("be.visible").click();
-    //  cy.url().should("include", "/login");
-    //  cy.contains("Logga in");
   });
 
-  // it("should log out a user successfully", () => {
-  //   cy.visit("http://localhost:3000/login");
+  beforeEach(() => {
+    cy.session("user-session", () => {
+      cy.visit("http://localhost:3000/login");
+      cy.get(":nth-child(2) > .login-input").type("user1@example.com");
+      // Fyll i lösenord
+      cy.get(":nth-child(3) > .login-input").type("user1*123");
+      // Klicka på logga in-knappen
+      cy.get(".login-button").click();
+      cy.wait(500);
+      // Klicka på logga in-knappen
+      cy.get(".login-button").click();
+      // Verifiera att användaren är omdirigerad till dashboard eller en annan skyddad sida
+      cy.url().should("include", "/profile");
 
-  //   cy.get(":nth-child(2) > .login-input").type("private@private.private");
-  //   // Fyll i lösenord
-  //   cy.get(":nth-child(3) > .login-input").type("Private1!");
-  //   // Klicka på logga in-knappen
-  //   cy.get(".login-button").click();
-  //   // Verifiera att användaren är omdirigerad till dashboard eller en annan skyddad sida
-  //   cy.url().should("include", "/profile");
+      // Verifiera att en viss text eller element finns på sidan, vilket bekräftar att användaren är inloggad
+      cy.contains("Min profil");
+    });
+  });
+  it("should log out a user successfully", () => {
+    cy.visit("http://localhost:3000");
 
-  //   // Verifiera att en viss text eller element finns på sidan, vilket bekräftar att användaren är inloggad
-  //   cy.contains("Min profil");
-  // });
+    cy.get(".burgerIcon").should("not.be.visible");
+
+    cy.get(".navbarMenu")
+      .should("be.visible")
+      .within(() => {
+        cy.contains("Logga in").should("not.exist");
+        cy.contains("Logga ut").should("exist");
+      });
+
+    // Klicka på "Logga in"-knappen
+    cy.contains("Logga ut").click();
+    cy.wait(800);
+    cy.url().should("include", "/");
+    cy.contains("Sök");
+
+    cy.get(".navbarMenu")
+      .should("be.visible")
+      .within(() => {
+        cy.contains("Logga ut").should("not.exist");
+        cy.contains("Logga in").should("exist");
+        cy.contains("Registrering").should("exist");
+      });
+  });
 });
