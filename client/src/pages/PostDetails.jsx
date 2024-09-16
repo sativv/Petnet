4462a832ea898d39f111c87e6ffcea605640f09c
David Vitas <david.vitas@hotmail.com>
2024-09-16 09:56:06 +0200

    adding nullcheck

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -286,7 +286,7 @@ function PostDetails() {
           </div>
         )}
 
-        {currentUser.id !== post.applicationUserId && (
+        {currentUser && currentUser.id !== post.applicationUserId && (
           <div className="favoriteIcon favoriteDiv" onClick={toggleFavorite}>
             <p>Spara Annons</p>
             <FontAwesomeIcon
